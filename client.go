e0a57e63d025c30cd6287d879b1743e949620735
Anuar <anuar.ssn@gmail.com>
2020-05-27 01:35:03 +0300

    code refactor

--- client.go
+++ client.go
@@ -1,38 +1,23 @@
 package combgen
 
 func CalculateCombinationsCount(input []string, maxAllowedChars int, charRepeat bool) int {
-	combinationStruct := buildCombinationStruct(input, maxAllowedChars, charRepeat)
-	var result int
-	if !charRepeat {
-		combInterface :=  nonRepeatCombinations{combinationStruct}
-		result =  combInterface.calculateNrOfCombinations() 
-	} else {
-		combInterface :=  charRepeatCombinations{combinationStruct}
-		result =  combInterface.calculateNrOfCombinations()
-	}	
-	
-	return result
+	combInterface := getCombInterface(input, maxAllowedChars, charRepeat)
+
+	return combInterface.calculateNrOfCombinations()
 }
 
 func CalculateCombinations(input []string, maxAllowedChars int, charRepeat bool) []string {
+	combInterface := getCombInterface(input, maxAllowedChars, charRepeat)
+
+	return combInterface.calculateCombinations()
+}
+
+func getCombInterface(input []string, maxAllowedChars int, charRepeat bool) combgenInterface {
 	combinationStruct := buildCombinationStruct(input, maxAllowedChars, charRepeat)
-	var result []string
+
 	if !charRepeat {
-		combInterface :=  nonRepeatCombinations{combinationStruct}
-		result = combInterface.calculateCombinations()
-	} else {
-		combInterface :=  charRepeatCombinations{combinationStruct}
-		result =  combInterface.calculateCombinations()
-	}	
+		return nonRepeatCombinations{combinationStruct}
+	}
 
-	return result
-}
-
-
-
-
-// TODO functionality
-func GetCombinationsCharRepeat(input []string, maxAllowedChars int) {
-}
-func GetNrOfCombinationsCharRepeat(input []string, maxAllowedChars int) {
+	return charRepeatCombinations{combinationStruct}
 }
